7d3076f04fd76c980e07a181f8ededcb621e6bdb
Rui Lopes <rgl@ruilopes.com>
2023-02-27 20:01:42 +0000

    upgrade to terraform 1.3.9

--- provision-tools.sh
+++ provision-tools.sh
@@ -6,14 +6,9 @@ sudo apt-get install -y apt-transport-https make unzip jq
 
 # install terraform.
 # see https://www.terraform.io/downloads.html
-artifact_url=https://releases.hashicorp.com/terraform/1.3.6/terraform_1.3.6_linux_amd64.zip
-artifact_sha=bb44a4c2b0a832d49253b9034d8ccbd34f9feeb26eda71c665f6e7fa0861f49b
+artifact_url=https://releases.hashicorp.com/terraform/1.3.9/terraform_1.3.9_linux_amd64.zip
 artifact_path="/tmp/$(basename $artifact_url)"
 wget -qO $artifact_path $artifact_url
-if [ "$(sha256sum $artifact_path | awk '{print $1}')" != "$artifact_sha" ]; then
-    echo "downloaded $artifact_url failed the checksum verification"
-    exit 1
-fi
 sudo unzip -o $artifact_path -d /usr/local/bin
 rm $artifact_path
 CHECKPOINT_DISABLE=1 terraform version
