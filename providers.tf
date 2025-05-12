db94570fabf33fc7ad23447d0a8b7df5e8b233bc
Rui Lopes <rgl@ruilopes.com>
2025-05-12 20:08:56 +0100

    upgrade dependencies

--- providers.tf
+++ providers.tf
@@ -1,24 +1,24 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.11.1"
+  required_version = "1.11.4"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
     random = {
       source  = "hashicorp/random"
-      version = "3.7.1"
+      version = "3.7.2"
     }
     # see https://registry.terraform.io/providers/hashicorp/cloudinit
     # see https://github.com/hashicorp/terraform-provider-cloudinit
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "2.3.6"
+      version = "2.3.7"
     }
     # see https://registry.terraform.io/providers/hashicorp/aws
     # see https://github.com/hashicorp/terraform-provider-aws
     aws = {
       source  = "hashicorp/aws"
-      version = "5.90.0"
+      version = "5.97.0"
     }
   }
 }
