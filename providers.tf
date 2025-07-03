07c7a711ac9f898eaf719c4aa8e09670e7dfb46c
Rui Lopes <rgl@ruilopes.com>
2025-07-03 19:00:00 +0000

    upgrade dependencies

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 # see https://github.com/hashicorp/terraform
 terraform {
-  required_version = "1.11.4"
+  required_version = "1.12.2"
   required_providers {
     # see https://registry.terraform.io/providers/hashicorp/random
     # see https://github.com/hashicorp/terraform-provider-random
@@ -18,7 +18,7 @@ terraform {
     # see https://github.com/hashicorp/terraform-provider-aws
     aws = {
       source  = "hashicorp/aws"
-      version = "5.97.0"
+      version = "6.2.0"
     }
   }
 }
